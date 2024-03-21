746445fbfd46a387fc208234e005d33fdda74e07
jetjo <bingsea@foxmail.com>
2024-03-21 10:34:47 +0800

    添加样式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./pos.css";
+import "./layout.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
