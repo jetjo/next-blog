9fe2df84249be2379abebf4a20b5639797ca6e64
jetjo <bingsea@foxmail.com>
2024-03-22 15:25:20 +0800

    在next.config.js中配置路由

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    postStorePath: path.resolve(__dirname, "./data/blogs"),
+    db_mongoDB: "mongodb://localhost:27017",
+    dbName: "test",
+  },
+//   运行在`Middleware`之前
+  async redirects() {
+    return [
+      {
+        source: "/:local/client",
+        destination: "/client",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
